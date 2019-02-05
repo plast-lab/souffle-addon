b54cdb0e2de061a92769d6bffce0432f846ff058
Yannis Smaragdakis <yannis@smaragd.org>
2019-02-05 14:40:19 +0200

    fix bugs based on unit tests failing

--- num256.cpp
+++ num256.cpp
@@ -7,7 +7,13 @@ using namespace boost::multiprecision;
 #define STRING_LEN 67 // 2 (for "0x") + 1 (for '\0') + 64 bytes for
 					  // the 32 bytes of data encoded in hex
 
+
 extern "C" {
+
+// Output always normalized to have:
+// - "0x" as prefix
+// - lower case letters for the hex digits (a-f)
+// - no leading zeros (i.e., "0xff", not "0x000ff")
 
 	// REVIEW: Lots of repetition, factor out in the future
 	
@@ -144,13 +150,18 @@ extern "C" {
 		return out;
 	}
 
-	// The next operations are for signed arithmetic, otherwise identical to above
+	// The next operations are for signed arithmetic, otherwise
+	// identical to above.  Note the need to transform the result to
+	// an unsigned int before converting to hex, since hex-conversion
+	// for negative numbers is not supported by the Boost
+	// multiprecision library.
 	const char* smod_256(const char *x, const char *y) {
 		thread_local static char out[STRING_LEN] = {"0x"}; 
 		int256_t my_x(x);
 		int256_t my_y(y);
 		int256_t result = my_x % my_y;
-		std::string str_result = result.str(32, std::ios_base::hex);
+		uint256_t unsigned_result(result);
+		std::string str_result = unsigned_result.str(32, std::ios_base::hex);
 		std::transform(str_result.begin(), str_result.end(), str_result.begin(), ::tolower);
 		strcpy(out+2, str_result.c_str());
 		return out;
@@ -161,7 +172,8 @@ extern "C" {
 		int256_t my_x(x);
 		int256_t my_y(y);
 		int256_t result = my_x / my_y;
-		std::string str_result = result.str(32, std::ios_base::hex);
+		uint256_t unsigned_result(result);
+		std::string str_result = unsigned_result.str(32, std::ios_base::hex);
 		std::transform(str_result.begin(), str_result.end(), str_result.begin(), ::tolower);
 		strcpy(out+2, str_result.c_str());
 		return out;
@@ -172,7 +184,8 @@ extern "C" {
 		int256_t my_x(x);
 		int256_t my_y(y);
 		int256_t result = my_x > my_y;
-		std::string str_result = result.str(32, std::ios_base::hex);
+		uint256_t unsigned_result(result);
+		std::string str_result = unsigned_result.str(32, std::ios_base::hex);
 		std::transform(str_result.begin(), str_result.end(), str_result.begin(), ::tolower);
 		strcpy(out+2, str_result.c_str());
 		return out;
@@ -183,7 +196,8 @@ extern "C" {
 		int256_t my_x(x);
 		int256_t my_y(y);
 		int256_t result = my_x < my_y;
-		std::string str_result = result.str(32, std::ios_base::hex);
+		uint256_t unsigned_result(result);
+		std::string str_result = unsigned_result.str(32, std::ios_base::hex);
 		std::transform(str_result.begin(), str_result.end(), str_result.begin(), ::tolower);
 		strcpy(out+2, str_result.c_str());
 		return out;
@@ -216,11 +230,22 @@ extern "C" {
 
 	// NOTE: INVERTED ARGUMENT ORDER!
 	const char* sar_256(const char *x, const char *y) {
-		thread_local static char out[STRING_LEN] = {"0x"}; 
+		thread_local static char out[STRING_LEN] = {"0x"};
 		int256_t my_x(y);
+		int256_t minus_zero = ((int256_t) 1) << 255;
+		int256_t sign_bit = my_x & minus_zero;
 		long my_y = strtol(x, NULL, 16);
-		int256_t result = my_x >> my_y;
-		std::string str_result = result.str(32, std::ios_base::hex);
+		int256_t result;
+		if (my_y >= 256) {
+			if (sign_bit) result = -1;
+			else result = 0;
+		} else {  // there must be a better way to implement SAR w/ Boost, but who cares?
+			for (result = my_x; my_y > 0; my_y--) {
+				result = (result >> 1) | sign_bit;
+			}
+		}
+		uint256_t unsigned_result(result);
+		std::string str_result = unsigned_result.str(32, std::ios_base::hex);
 		std::transform(str_result.begin(), str_result.end(), str_result.begin(), ::tolower);
 		strcpy(out+2, str_result.c_str());
 		return out;
